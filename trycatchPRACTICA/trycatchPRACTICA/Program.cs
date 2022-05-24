1e8a37086b1fa3c1a70a00b1c46446875c9a9edb
JorgeGutierrezP <105743070+JorgeGutierrezP@users.noreply.github.com>
2022-05-24 11:30:34 -0700

    Version 1

---
+++ trycatchPRACTICA/trycatchPRACTICA/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trycatchPRACTICA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            try
+            {
+                Console.Write("Ingresar un número: ");
+                num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("El cuadrado del número es: " + Math.Pow(num, 2));
+                Console.ReadKey();
+            }
+            catch(FormatException m)
+            {
+                Console.WriteLine("Error: " + m.Message);
+            }
+            catch (OverflowException m)
+            {
+                Console.WriteLine("Error: " + m.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
